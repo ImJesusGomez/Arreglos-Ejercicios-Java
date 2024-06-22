380cfdb9c0b24371f1f5068ac6533dad2847dc67
ImJesusGomez <jesusgomez.dev10@gmail.com>
2024-06-22 14:30:00 -0600

    Segundo Ejercicio terminado

---
+++ src/arreglosejercicios/MostrarDatosUsuario.java
@@ -0,0 +1,49 @@
+/*
+INSTRUCCIONES: En un arreglo, mostrar los datos de un usuario ( nombre, edad y sexo).
+*/
+package arreglosejercicios;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MostrarDatosUsuario {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+        
+        // Creamos los arreglos que almacenarán los datos
+        String[] nombre;
+        int[] edad;
+        char[] sexo;
+        
+        // Preguntamos cuantos usuario se van a registrar
+        System.out.println("Cuantos usuarios se van a registrar? ");
+        int cantUsuarios = Integer.parseInt(teclado.readLine());
+        
+        
+        // Establecemos el tamaño del arreglo
+        nombre = new String[cantUsuarios];
+        edad = new int[cantUsuarios];
+        sexo = new char[cantUsuarios];
+        
+        // Llenamos de datos los arreglos
+        for(int i = 0; i < nombre.length; i++){
+            System.out.println("Ingresa el nombre: ");
+            nombre[i] = teclado.readLine();
+            System.out.println("Ingresa la edad: ");
+            edad[i] = Integer.parseInt(teclado.readLine());
+            System.out.println("Ingresa el sexo: (M = Masculino/F = Femenino)");
+            sexo[i] = teclado.readLine().charAt(0);
+        }
+        
+        // Mostramos los datos
+        System.out.print("Nombre      Edad        Sexo");
+        System.out.println("_____       ____        ____");
+        for(int i = 0; i < nombre.length; i++){
+            System.out.println(nombre[i] + "\t" + edad[i] + "\t" + sexo[i]);
+        }
+        
+    }
+    
+}
