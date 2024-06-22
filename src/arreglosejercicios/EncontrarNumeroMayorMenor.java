4ba9aa54a2e8e3913049b4c61bcfde4d3849e96d
ImJesusGomez <jesusgomez.dev10@gmail.com>
2024-06-22 15:04:14 -0600

    Cuarto Ejercicio terminado

---
+++ src/arreglosejercicios/EncontrarNumeroMayorMenor.java
@@ -0,0 +1,43 @@
+/*
+INSTRUCCIONES: Ingresar por teclado 10 números en un arreglo y mostrar el numero mayor y el numero menor.
+*/
+
+package arreglosejercicios;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class EncontrarNumeroMayorMenor {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+        
+        // Declaramos el arreglo
+        int[] numeros = new int[10];
+        
+        // Llenamos el arreglo de valores
+        for(int i = 0; i < numeros.length; i++){
+            System.out.print("Ingrese un numero: ");
+            numeros[i] = Integer.parseInt(teclado.readLine());
+        }
+        
+        // Comparamos cual es el menor y cual el mayor
+        int numMenor = numeros[0];
+        int numMayor = numeros[0];
+        
+        for(int i = 0; i < numeros.length; i++){
+            if(numeros[i] > numMayor){
+                numMayor = numeros[i];
+            }
+            
+            if(numeros[i] < numMenor){
+                numMenor = numeros[i];
+            }
+        }
+        
+        System.out.println("Numero Mayor: " + numMayor);
+        System.out.println("Numero Menor: " + numMenor);
+    }
+    
+}
